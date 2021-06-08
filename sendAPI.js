97099c2d339dfd701648d45aff61636a0ab8773f
mewtprtn <60011275@kmitl.ac.th>
2021-06-08 19:13:19 +0700

    Update Change API

--- sendAPI.js
+++ sendAPI.js
@@ -2,8 +2,8 @@ function sendAPI() {
     var videoname = document.getElementById('fn').value;
     var url = document.getElementById('url1').value;
     var token = "cpp6z10cavht5PDMzzGkeJdtJaB2";
-    var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
-    var theUrl = "http://52.237.127.46:5000/VQRaddAPI";
+    var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance http://52.237.127.46:5000/VQRaddA
+    var theUrl = "http://52.237.127.46:5000/VQRaddA";
     xmlhttp.open("POST", theUrl);
     //xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     var payload = { vid_path: videoname , longlink: url , access_token: token }
