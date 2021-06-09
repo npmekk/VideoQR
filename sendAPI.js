39fda2deb66efeed5e64791e1312354d28b754ad
mewtprtn <60011275@kmitl.ac.th>
2021-06-09 15:40:38 +0700

    Update fix sendAPI

--- sendAPI.js
+++ sendAPI.js
@@ -1,12 +1,13 @@
 function sendAPI() {
     var videoname = document.getElementById('fn').value;
     var url = document.getElementById('url1').value;
-    var token = "cpp6z10cavht5PDMzzGkeJdtJaB2";
+    var token = azureuid;//cpp6z10cavht5PDMzzGkeJdtJaB2
     var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance http://52.237.127.46:5000/VQRaddA
     var theUrl = "http://52.237.127.46:5000/VQRaddA";
-    xmlhttp.open("POST", theUrl);
+    //xmlhttp.open("POST", theUrl);
     //xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     var payload = { vid_path: videoname , longlink: url , access_token: token }
-    xmlhttp.send(JSON.stringify(payload));
+    //xmlhttp.send(JSON.stringify(payload));
+    JSON.stringify(payload)
     console.log(payload);
 }
